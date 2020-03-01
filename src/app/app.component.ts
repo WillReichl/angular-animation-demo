c0b6992772ed051f46d0465ca47a24083be91f30
Will Reichl <William.Reichl@dcsg.com>
2020-03-01 13:47:47 -0500

    Switching between states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,34 @@ import { trigger, state, style } from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations: [trigger('divState', [
-    state('normal', style({
-      'background-color': 'red',
-      transform: 'translateX(0)'
-    })),
-    state('highlighted', style({
-      'background-color': 'blue',
-      transform: 'translateX(100)'
-    }))
-  ])]
+  animations: [
+    trigger('divState', [
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0)'
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          'background-color': 'blue',
+          transform: 'translateX(100px)'
+        })
+      )
+    ])
+  ]
 })
 export class AppComponent {
   state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state = this.state === 'normal'
+      ? 'highlighted'
+      : 'normal';
+  }
 
   onAdd(item) {
     this.list.push(item);
